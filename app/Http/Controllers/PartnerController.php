4b3479950ca661559389c554a411ff7781108305
Sumon Ahmed <bdsumon4u@gmail.com>
2021-05-29 16:10:54 +0600

    Partners Only BelongTo Own Country

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -23,7 +23,7 @@ class PartnerController extends Controller
     public function index(Builder $builder)
     {
         $query = User::query()
-            #->where('country', request()->user()->country)
+            ->where('country', request()->user()->country)
             ->whereHas('partner', function ($query) {
                 return $query->approved();
             });
